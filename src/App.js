fbf54257abf12820e302ba0e5fc2cfcf7933a8c1
GreenCatInForest <event2016.2017@gmail.com>
2023-11-30 13:57:29 +0000

    add background

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import Container from "react-bootstrap/Container";
 import Stack from "react-bootstrap/Stack";
 
+import { Banner } from "./components/Banner";
+import CloudBannerImg from "./assets/images/clouds.jpg";
+
 export const App = () => {
   return (
-    <Container>
+    <Container
+      className="bg-dark vh-100 text-light"
+      style={{
+        backgroundImage: `url(${CloudBannerImg}`,
+        backgroundSize: "cover",
+        opacity: "80%",
+      }}
+    >
+      <Banner />
       <Stack>
         <div className="p-2">First item</div>
         <div className="p-2">Second item</div>
---
+++ src/components/Banner.js
@@ -0,0 +1,10 @@
+import Stack from "react-bootstrap/Stack";
+
+export const Banner = () => {
+  return (
+    <Stack>
+      <h1 className="mx-auto">Title</h1>
+      <h2 className="mx-auto">Subtitle</h2>
+    </Stack>
+  );
+};
