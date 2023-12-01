43127d284b12a36481160c938f92ace337264fd1
GreenCatInForest <event2016.2017@gmail.com>
2023-12-01 16:41:02 +0000

    add onClick handle searchMode currentTarget

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -9,16 +9,31 @@ export const SearchForm = () => {
   const [searchMode, setSearchMode] = useState("organisation");
   const [searchTerm, setSearchTerm] = useState("");
 
+  const handleSearchMode = ({ currentTarget }) =>
+    setSearchMode(currentTarget.name);
+
   return (
     <Form className="p-3">
       <Stack gap={3}>
         <ButtonGroup aria-label="Basic example">
-          <Button variant="secondary active">Search by Organisation</Button>
-          <Button variant="secondary">Search by Username</Button>
+          <Button
+            variant="secondary active"
+            name="organisation"
+            onClick={handleSearchMode}
+          >
+            Search by Organisation
+          </Button>
+          <Button
+            variant="secondary"
+            name="username"
+            onClick={handleSearchMode}
+          >
+            Search by Username
+          </Button>
         </ButtonGroup>
 
         <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Control type="text" placeholder={`Enter the {searchMode}`} />
+          <Form.Control type="text" placeholder={`Enter the ${searchMode}`} />
           <Form.Text className="text-danger">
             Please enter a valid organisation
           </Form.Text>
